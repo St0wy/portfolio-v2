c78e24e9c5d91d4c4a2335dc3fabbed4719220e7
Stowy <stowy@protonmail.ch>
2023-02-10 17:42:57 +0100

    removed useless stuff and added projects content structure

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,6 +1,6 @@
 import { z, defineCollection } from "astro:content";
 
-const blogSchema = z.object({
+const projectSchema = z.object({
     title: z.string(),
     description: z.string(),
     pubDate: z.coerce.date(),
@@ -8,27 +8,10 @@ const blogSchema = z.object({
     heroImage: z.string().optional(),
 });
 
-const storeSchema = z.object({
-    title: z.string(),
-    description: z.string(),
-    details: z.boolean().optional(),
-    custom_link_label: z.string(),
-    custom_link: z.string().optional(),
-    updatedDate: z.coerce.date(),
-    pricing: z.string().optional(),
-    oldPricing:  z.string().optional(),
-    badge: z.string().optional(),
-    checkoutUrl: z.string().optional(),
-    heroImage: z.string().optional(),
-});
+export type ProjectSchema = z.infer<typeof projectSchema>;
 
-export type BlogSchema = z.infer<typeof blogSchema>;
-export type StoreSchema = z.infer<typeof storeSchema>;
-
-const blogCollection = defineCollection({ schema: blogSchema });
-const storeCollection = defineCollection({ schema: storeSchema });
+const projectCollection = defineCollection({ schema: projectSchema });
 
 export const collections = {
-    'blog': blogCollection,
-    'store': storeCollection
+    'projects': projectCollection,
 }
