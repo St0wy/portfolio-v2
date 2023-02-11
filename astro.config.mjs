fa0af682ccaacedb01e4eaf0f32bd5ead758fad2
Stowy <stowy@protonmail.ch>
2023-02-11 17:01:08 +0100

    added astro publish

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,9 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
-
-// https://astro.build/config
 export default defineConfig({
-  site: 'https://astro-modern-personal-website.netlify.app',
-  integrations: [mdx(), sitemap(), tailwind()]
+	site: "https://fabianhbr.ch",
+	integrations: [mdx(), sitemap(), tailwind()],
 });
