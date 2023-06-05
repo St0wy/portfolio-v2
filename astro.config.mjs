23da3e88dba5c6f42d5f3700bcd0c75c258f3593
Stowy <stowy@protonmail.ch>
2023-06-05 16:05:44 +0200

    Tried to fix build

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://fabianhbr.ch",
+	site: "https://www.fabianhbr.ch",
 	integrations: [mdx(), sitemap(), tailwind()],
 });
