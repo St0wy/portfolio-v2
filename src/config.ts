4b6561484ce6512a95bdf4ae7693a71e832efaef
Stowy <stowy@protonmail.ch>
2023-08-31 12:33:36 +0200

    Fixed typo

--- src/config.ts
+++ src/config.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Fabian Huber Portfolio';
-export const SITE_DESCRIPTION = 'My Games Programmer portfolio.';
+export const SITE_DESCRIPTION = 'My Game Programmer portfolio.';
